fcc9c6c10754fd4f4784484181beaa6dfe327615
Sesilyo <c.sethleander@gmail.com>
2026-04-20 13:01:39 +0800

    added date and time display

--- web/scripts/dateTimeLogics.js
+++ web/scripts/dateTimeLogics.js
@@ -1,5 +1,29 @@
 /* DISPLAY MAIN TIME BLOCK */
+const dateDisplayer = document.getElementById("date-displayer");
+const timeDisplayer = document.getElementById("time-displayer");
+
 setInterval(() => {
     const now = new Date();
-    
+
+    // customized format for day & day
+    const dateFormat = {
+        weekday: 'long',
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric'
+    };
+
+    const formattedDate = now.toLocaleDateString('en-US', dateFormat)
+        .replace(',', '-')
+        .replaceAll('/', '-');
+
+    // getting time
+    const hh = String(now.getHours()).padStart(2,   '0');
+    const mm = String(now.getMinutes()).padStart(2, '0');
+    const ss = String(now.getSeconds()).padStart(2, '0');
+
+    const formattedTime = `${hh}:${mm}:${ss}`;
+
+    timeDisplayer.textContent = formattedTime;
+    dateDisplayer.textContent = formattedDate;
 }, 1000);
